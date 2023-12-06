147135c3292197803fc6787c089a0347c9a23ca0
unknown <chingu647@gmail.com>
2023-12-06 21:09:43 +0900

    app 2

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
 import streamlit as st 
 
-st.text("hello streamlit!!")
-st.text("hello git")
-st.text("hello git")
-st.text("hello git")
-st.text("hello git")
-st.text("hello git")
+def main(): 
+    st.title("서울시 아파트 가격 추세분석 및 예측")
+    name = '장인구' 
+    st.text(f"제 이름은 {name}입니다.")
+    st.header(f"이 영역은 헤더")
+    st.subheader(f"이 영역은 서브헤더")
+
+    st.success(f"성공했을때") 
+    st.error(f"에러일때")
+    st.warning(f"경고 일때")
+    st.info(f"정보를 줄때") 
+
+    st.help(sum) 
+    st.help(len)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__': 
+    main()
+
