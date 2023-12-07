31bd04cf05e5d4c02738503a3fa02612dfede7b5
unknown <chingu647@gmail.com>
2023-12-07 11:30:13 +0900

    all ...

--- app.py
+++ app.py
@@ -10,20 +10,18 @@ from collections import Counter
 
 from PIL import Image 
 
-def main(): 
-    col1,col2 = st.columns([2,3])
-    with col1:
-        st.title("col1")
-    with col2:
-        st.title("col2")
-        st.checkbox("this is checkbox1", key="chk1") 
-        st.checkbox("this is checkbox2", key="chk2")       
+col1,col2 = st.columns([2,3])
+with col1:
+    st.title("col1")
+with col2:
+    st.title("col2")
+    chk1 = st.checkbox("this is checkbox1") 
+    chk2 = st.checkbox("this is checkbox2")       
 
-    st.sidebar.title("this is sidebar")
-    st.sidebar.checkbox("체크박스 문구", key="chk3")
+st.sidebar.title("this is sidebar")
+chk3 = st.sidebar.checkbox("체크박스 문구") 
 
 
-main()
 
 
 
