cc8c1f930873a84cdbeb39aa6b9f7ebf55707c23
unknown <chingu647@gmail.com>
2023-12-07 11:23:26 +0900

    all ...

--- app.py
+++ app.py
@@ -23,23 +23,7 @@ def main():
     st.sidebar.checkbox("체크박스 문구")
 
 
-
-
-
-#    st.title("서울시 아파트 가격 추세분석 및 예측")
-#    name = '장인구' 
-#    st.text(f"제 이름은 {name}입니다.")
-#    st.header(f"이 영역은 헤더")
-#    st.subheader(f"이 영역은 서브헤더")
-
-#    st.success(f"성공했을때") 
-#    st.error(f"에러일때")
-#    st.warning(f"경고 일때")
-#    st.info(f"정보를 줄때") 
-
-#    st.help(sum) 
-#    st.help(len)
-
+main()
 
 
 
